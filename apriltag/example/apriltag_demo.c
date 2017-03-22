2f5dcf7ccb1a15336883f209d2e0d114b2dc5f49
TroyCordie <troy.cordie@connect.qut.edu.au>
2017-03-23 08:17:52 +1000

    apriltag code ready for testing on the pi

---
+++ apriltag/example/apriltag_demo.c
@@ -0,0 +1,579 @@
+/* Copyright (C) 2013-2016, The Regents of The University of Michigan.
+All rights reserved.
+
+This software was developed in the APRIL Robotics Lab under the
+direction of Edwin Olson, ebolson@umich.edu. This software may be
+available under alternative licensing terms; contact the address above.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies,
+either expressed or implied, of the Regents of The University of Michigan.
+*/
+
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include "apriltag.h"
+#include "tag36h11.h"
+#include "tag36h10.h"
+#include "tag36artoolkit.h"
+#include "tag25h9.h"
+#include "tag25h7.h"
+
+#include "common/getopt.h"
+#include "common/image_u8.h"
+#include "common/image_u8x4.h"
+#include "common/pjpeg.h"
+#include "common/zarray.h"
+
+//########capture image
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <asm/types.h>
+#include <linux/videodev2.h>
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <jpeglib.h>
+
+
+void quit();
+
+int xioctl();
+
+typedef struct {
+  uint8_t* start;
+  size_t length;
+} buffer_t;
+
+typedef struct {
+  int fd;
+  uint32_t width;
+  uint32_t height;
+  size_t buffer_count;
+  buffer_t* buffers;
+  buffer_t head;
+} camera_t;
+
+camera_t* camera_open();
+
+void camera_init();
+
+void camera_start();
+
+void camera_stop();
+
+void camera_finish();
+
+void camera_close();
+
+int camera_capture();
+
+int camera_frame();
+
+void jpeg();
+
+uint8_t* yuyv2rgb();
+
+int minmax(int min, int v, int max);
+
+// Invoke:
+//
+// tagtest [options] input.pnm
+
+int main(int argc, char *argv[])
+{
+
+    getopt_t *getopt = getopt_create();
+
+    getopt_add_bool(getopt, 'h', "help", 0, "Show this help");
+    getopt_add_bool(getopt, 'd', "debug", 0, "Enable debugging output (slow)");
+    getopt_add_bool(getopt, 'q', "quiet", 0, "Reduce output");
+    getopt_add_string(getopt, 'f', "family", "tag36h11", "Tag family to use");
+    getopt_add_int(getopt, '\0', "border", "1", "Set tag family border size");
+    getopt_add_int(getopt, 'i', "iters", "1", "Repeat processing on input set this many times");
+    getopt_add_int(getopt, 't', "threads", "4", "Use this many CPU threads");
+    getopt_add_double(getopt, 'x', "decimate", "1.0", "Decimate input image by this factor");
+    getopt_add_double(getopt, 'b', "blur", "0.0", "Apply low-pass blur to input; negative sharpens");
+    getopt_add_bool(getopt, '0', "refine-edges", 1, "Spend more time trying to align edges of tags");
+    getopt_add_bool(getopt, '1', "refine-decode", 0, "Spend more time trying to decode tags");
+    getopt_add_bool(getopt, '2', "refine-pose", 0, "Spend more time trying to precisely localize tags");
+
+    if (!getopt_parse(getopt, argc, argv, 1) || getopt_get_bool(getopt, "help")) {
+        printf("Usage: %s [options] <input files>\n", argv[0]);
+        getopt_do_usage(getopt);
+        exit(0);
+    }
+
+    const zarray_t *inputs = getopt_get_extra_args(getopt);
+
+    apriltag_family_t *tf = NULL;
+    const char *famname = getopt_get_string(getopt, "family");
+    if (!strcmp(famname, "tag36h11"))
+        tf = tag36h11_create();
+    else if (!strcmp(famname, "tag36h10"))
+        tf = tag36h10_create();
+    else if (!strcmp(famname, "tag36artoolkit"))
+        tf = tag36artoolkit_create();
+    else if (!strcmp(famname, "tag25h9"))
+        tf = tag25h9_create();
+    else if (!strcmp(famname, "tag25h7"))
+        tf = tag25h7_create();
+    else {
+        printf("Unrecognized tag family name. Use e.g. \"tag36h11\".\n");
+        exit(-1);
+    }
+
+    tf->black_border = getopt_get_int(getopt, "border");
+
+    apriltag_detector_t *td = apriltag_detector_create();
+    apriltag_detector_add_family(td, tf);
+    td->quad_decimate = getopt_get_double(getopt, "decimate");
+    td->quad_sigma = getopt_get_double(getopt, "blur");
+    td->nthreads = getopt_get_int(getopt, "threads");
+    td->debug = getopt_get_bool(getopt, "debug");
+    td->refine_edges = getopt_get_bool(getopt, "refine-edges");
+    td->refine_decode = getopt_get_bool(getopt, "refine-decode");
+    td->refine_pose = getopt_get_bool(getopt, "refine-pose");
+
+    int quiet = getopt_get_bool(getopt, "quiet");
+
+    int maxiters = getopt_get_int(getopt, "iters");
+
+    const int hamm_hist_max = 10;
+
+
+
+    for (int iter = 0; iter < maxiters; iter++) {
+
+        int total_quads = 0;
+        int total_hamm_hist[hamm_hist_max];
+        memset(total_hamm_hist, 0, sizeof(total_hamm_hist));
+        double total_time = 0;
+
+        if (maxiters > 1)
+            printf("iter %d / %d\n", iter + 1, maxiters);
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+            camera_t* camera = camera_open("/dev/video0", 640 , 480);
+            camera_init(camera);
+            camera_start(camera);
+
+            struct timeval timeout;
+            timeout.tv_sec = 1;
+            timeout.tv_usec = 0;
+            /* skip 5 frames for booting a cam */
+            for (int i = 0; i < 5; i++) {
+              camera_frame(camera, timeout);
+            }
+            camera_frame(camera, timeout);
+
+            unsigned char* rgb = yuyv2rgb(camera->head.start, camera->width, camera->height);
+
+
+
+            FILE* out = fopen("result.jpg", "w");
+            jpeg(out, rgb, camera->width, camera->height, 100);
+            fclose(out);
+
+            camera_stop(camera);
+            camera_finish(camera);
+            camera_close(camera);
+            free(rgb);
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+//        for (int input = 0; input < zarray_size(inputs); input++) {
+
+
+            int hamm_hist[hamm_hist_max];
+            memset(hamm_hist, 0, sizeof(hamm_hist));
+
+            char path[12];
+            //zarray_get(inputs, input, &path);
+
+            strcpy(path, "result.jpg");
+            if (!quiet)
+                printf("loading %s\n", path);
+            else
+                printf("%20s ", path);
+
+            image_u8_t *im = NULL;
+
+            //int len = camera->width * camera->height * 3;
+
+            //im = image_u8_create_stride(camera->width, camera->height, camera->width*3);
+            //im->stride = camera->width*3;
+
+            /*im = image_u8_create(camera->width, camera->height);
+
+            for (int y = 0; y < camera->height; y++) {
+                for (int x = 0; x < camera->width; x++) {
+                    double r = rgb[y*camera->width + 3*x + 0] / 255.0;
+                    double g = rgb[y*camera->width + 3*x + 1] / 255.0;
+                    double b = rgb[y*camera->width + 3*x + 2] / 255.0;
+
+                    double alpha = 0.42;
+                    double v = 0.5 + log(g) - alpha*log(b) - (1-alpha)*log(r);
+                    uint8_t iv = v * 255;
+                    if (iv < 0)
+                        iv = 0;
+                    if (iv > 255)
+                        iv = 255;
+
+                    im->buf[y*im->stride + x] = iv;
+                }
+            }*/
+
+            if (str_ends_with(path, "pnm") || str_ends_with(path, "PNM") ||
+                str_ends_with(path, "pgm") || str_ends_with(path, "PGM"))
+                im = image_u8_create_from_pnm(path);
+            else if (str_ends_with(path, "jpg") || str_ends_with(path, "JPG")) {
+                int err = 0;
+                pjpeg_t *pjpeg = pjpeg_create_from_file(path, 0, &err);
+                if (pjpeg == NULL) {
+                    printf("pjpeg error %d\n", err);
+                    continue;
+                }
+
+                if (1) {
+                    im = pjpeg_to_u8_baseline(pjpeg);
+                } else {
+                    printf("illumination invariant\n");
+
+                    image_u8x3_t *imc =  pjpeg_to_u8x3_baseline(pjpeg);
+
+                    im = image_u8_create(imc->width, imc->height);
+
+                    for (int y = 0; y < imc->height; y++) {
+                        for (int x = 0; x < imc->width; x++) {
+                            double r = imc->buf[y*imc->stride + 3*x + 0] / 255.0;
+                            double g = imc->buf[y*imc->stride + 3*x + 1] / 255.0;
+                            double b = imc->buf[y*imc->stride + 3*x + 2] / 255.0;
+
+                            double alpha = 0.42;
+                            double v = 0.5 + log(g) - alpha*log(b) - (1-alpha)*log(r);
+                            int iv = v * 255;
+                            if (iv < 0)
+                                iv = 0;
+                            if (iv > 255)
+                                iv = 255;
+
+                            im->buf[y*im->stride + x] = iv;
+                        }
+                    }
+                    image_u8x3_destroy(imc);
+                    if (td->debug)
+                        image_u8_write_pnm(im, "debug_invariant.pnm");
+                }
+
+                pjpeg_destroy(pjpeg);
+            }
+
+
+
+            if (im == NULL) {
+                printf("couldn't load %s\n", path);
+                continue;
+            }
+
+            zarray_t *detections = apriltag_detector_detect(td, im);
+
+            for (int i = 0; i < zarray_size(detections); i++) {
+                apriltag_detection_t *det;
+                zarray_get(detections, i, &det);
+
+                if (!quiet)
+                    printf("detection %3d: id (%2dx%2d)-%-4d, hamming %d, goodness %8.3f, margin %8.3f, x %8.3f\n",
+                           i, det->family->d*det->family->d, det->family->h, det->id, det->hamming, det->goodness, det->decision_margin, det->c[0]);
+
+                hamm_hist[det->hamming]++;
+                total_hamm_hist[det->hamming]++;
+            }
+
+            apriltag_detections_destroy(detections);
+
+            if (!quiet) {
+                timeprofile_display(td->tp);
+            }
+
+            total_quads += td->nquads;
+
+            if (!quiet)
+                printf("hamm ");
+
+            for (int i = 0; i < hamm_hist_max; i++)
+                printf("%5d ", hamm_hist[i]);
+
+            double t =  timeprofile_total_utime(td->tp) / 1.0E3;
+            total_time += t;
+            printf("%12.3f ", t);
+            printf("%5d", td->nquads);
+
+            printf("\n");
+
+            image_u8_destroy(im);
+        //}
+
+
+        printf("Summary\n");
+
+        printf("hamm ");
+
+        for (int i = 0; i < hamm_hist_max; i++)
+            printf("%5d ", total_hamm_hist[i]);
+        printf("%12.3f ", total_time);
+        printf("%5d", total_quads);
+        printf("\n");
+
+    }
+
+    // don't deallocate contents of inputs; those are the argv
+    apriltag_detector_destroy(td);
+
+    tag36h11_destroy(tf);
+    return 0;
+}
+
+void quit(const char * msg)
+{
+  fprintf(stderr, "[%s] %d: %s\n", msg, errno, strerror(errno));
+  exit(EXIT_FAILURE);
+}
+
+int xioctl(int fd, int request, void* arg)
+{
+  for (int i = 0; i < 100; i++) {
+    int r = ioctl(fd, request, arg);
+    if (r != -1 || errno != EINTR) return r;
+  }
+  return -1;
+}
+
+camera_t* camera_open(const char * device, uint32_t width, uint32_t height)
+{
+  int fd = open(device, O_RDWR | O_NONBLOCK, 0);
+  if (fd == -1) quit("open");
+  camera_t* camera = malloc(sizeof (camera_t));
+  camera->fd = fd;
+  camera->width = width;
+  camera->height = height;
+  camera->buffer_count = 0;
+  camera->buffers = NULL;
+  camera->head.length = 0;
+  camera->head.start = NULL;
+  return camera;
+}
+
+void camera_init(camera_t* camera) {
+  struct v4l2_capability cap;
+  if (xioctl(camera->fd, VIDIOC_QUERYCAP, &cap) == -1) quit("VIDIOC_QUERYCAP");
+  if (!(cap.capabilities & V4L2_CAP_VIDEO_CAPTURE)) quit("no capture");
+  if (!(cap.capabilities & V4L2_CAP_STREAMING)) quit("no streaming");
+
+  struct v4l2_cropcap cropcap;
+  memset(&cropcap, 0, sizeof cropcap);
+  cropcap.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+  if (xioctl(camera->fd, VIDIOC_CROPCAP, &cropcap) == 0) {
+    struct v4l2_crop crop;
+    crop.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    crop.c = cropcap.defrect;
+    if (xioctl(camera->fd, VIDIOC_S_CROP, &crop) == -1) {
+      // cropping not supported
+    }
+  }
+
+  struct v4l2_format format;
+  memset(&format, 0, sizeof format);
+  format.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+  format.fmt.pix.width = camera->width;
+  format.fmt.pix.height = camera->height;
+  format.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
+  format.fmt.pix.field = V4L2_FIELD_NONE;
+  if (xioctl(camera->fd, VIDIOC_S_FMT, &format) == -1) quit("VIDIOC_S_FMT");
+
+  struct v4l2_requestbuffers req;
+  memset(&req, 0, sizeof req);
+  req.count = 4;
+  req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+  req.memory = V4L2_MEMORY_MMAP;
+  if (xioctl(camera->fd, VIDIOC_REQBUFS, &req) == -1) quit("VIDIOC_REQBUFS");
+  camera->buffer_count = req.count;
+  camera->buffers = calloc(req.count, sizeof (buffer_t));
+
+  size_t buf_max = 0;
+  for (size_t i = 0; i < camera->buffer_count; i++) {
+    struct v4l2_buffer buf;
+    memset(&buf, 0, sizeof buf);
+    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    buf.memory = V4L2_MEMORY_MMAP;
+    buf.index = i;
+    if (xioctl(camera->fd, VIDIOC_QUERYBUF, &buf) == -1)
+      quit("VIDIOC_QUERYBUF");
+    if (buf.length > buf_max) buf_max = buf.length;
+    camera->buffers[i].length = buf.length;
+    camera->buffers[i].start =
+      mmap(NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED,
+           camera->fd, buf.m.offset);
+    if (camera->buffers[i].start == MAP_FAILED) quit("mmap");
+  }
+  camera->head.start = malloc(buf_max);
+}
+
+void camera_start(camera_t* camera)
+{
+  for (size_t i = 0; i < camera->buffer_count; i++) {
+    struct v4l2_buffer buf;
+    memset(&buf, 0, sizeof buf);
+    buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+    buf.memory = V4L2_MEMORY_MMAP;
+    buf.index = i;
+    if (xioctl(camera->fd, VIDIOC_QBUF, &buf) == -1) quit("VIDIOC_QBUF");
+  }
+
+  enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+  if (xioctl(camera->fd, VIDIOC_STREAMON, &type) == -1)
+    quit("VIDIOC_STREAMON");
+}
+
+void camera_stop(camera_t* camera)
+{
+  enum v4l2_buf_type type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+  if (xioctl(camera->fd, VIDIOC_STREAMOFF, &type) == -1)
+    quit("VIDIOC_STREAMOFF");
+}
+
+void camera_finish(camera_t* camera)
+{
+  for (size_t i = 0; i < camera->buffer_count; i++) {
+    munmap(camera->buffers[i].start, camera->buffers[i].length);
+  }
+  free(camera->buffers);
+  camera->buffer_count = 0;
+  camera->buffers = NULL;
+  free(camera->head.start);
+  camera->head.length = 0;
+  camera->head.start = NULL;
+}
+
+void camera_close(camera_t* camera)
+{
+  if (close(camera->fd) == -1) quit("close");
+  free(camera);
+}
+
+int camera_capture(camera_t* camera)
+{
+  struct v4l2_buffer buf;
+  memset(&buf, 0, sizeof buf);
+  buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+  buf.memory = V4L2_MEMORY_MMAP;
+  if (xioctl(camera->fd, VIDIOC_DQBUF, &buf) == -1) return FALSE;
+  memcpy(camera->head.start, camera->buffers[buf.index].start, buf.bytesused);
+  camera->head.length = buf.bytesused;
+  if (xioctl(camera->fd, VIDIOC_QBUF, &buf) == -1) return FALSE;
+  return TRUE;
+}
+
+int camera_frame(camera_t* camera, struct timeval timeout) {
+  fd_set fds;
+  FD_ZERO(&fds);
+  FD_SET(camera->fd, &fds);
+  int r = select(camera->fd + 1, &fds, 0, 0, &timeout);
+  if (r == -1) quit("select");
+  if (r == 0) return FALSE;
+  return camera_capture(camera);
+}
+
+void jpeg(FILE* dest, uint8_t* rgb, uint32_t width, uint32_t height, int quality)
+{
+  JSAMPARRAY image;
+  image = calloc(height, sizeof (JSAMPROW));
+  for (size_t i = 0; i < height; i++) {
+    image[i] = calloc(width * 3, sizeof (JSAMPLE));
+    for (size_t j = 0; j < width; j++) {
+      image[i][j * 3 + 0] = rgb[(i * width + j) * 3 + 0];
+      image[i][j * 3 + 1] = rgb[(i * width + j) * 3 + 1];
+      image[i][j * 3 + 2] = rgb[(i * width + j) * 3 + 2];
+    }
+  }
+
+  struct jpeg_compress_struct compress;
+  struct jpeg_error_mgr error;
+  compress.err = jpeg_std_error(&error);
+  jpeg_create_compress(&compress);
+  jpeg_stdio_dest(&compress, dest);
+
+  compress.image_width = width;
+  compress.image_height = height;
+  compress.input_components = 3;
+  compress.in_color_space = JCS_RGB;
+  jpeg_set_defaults(&compress);
+  jpeg_set_quality(&compress, quality, TRUE);
+  jpeg_start_compress(&compress, TRUE);
+  jpeg_write_scanlines(&compress, image, height);
+  jpeg_finish_compress(&compress);
+  jpeg_destroy_compress(&compress);
+
+  for (size_t i = 0; i < height; i++) {
+    free(image[i]);
+  }
+  free(image);
+}
+
+int minmax(int min, int v, int max)
+{
+  return (v < min) ? min : (max < v) ? max : v;
+}
+
+uint8_t* yuyv2rgb(uint8_t* yuyv, uint32_t width, uint32_t height)
+{
+  uint8_t* rgb = calloc(width * height * 3, sizeof (uint8_t));
+  for (size_t i = 0; i < height; i++) {
+    for (size_t j = 0; j < width; j += 2) {
+      size_t index = i * width + j;
+      int y0 = yuyv[index * 2 + 0] << 8;
+      int u = yuyv[index * 2 + 1] - 128;
+      int y1 = yuyv[index * 2 + 2] << 8;
+      int v = yuyv[index * 2 + 3] - 128;
+      rgb[index * 3 + 0] = minmax(0, (y0 + 359 * v) >> 8, 255);
+      rgb[index * 3 + 1] = minmax(0, (y0 + 88 * v - 183 * u) >> 8, 255);
+      rgb[index * 3 + 2] = minmax(0, (y0 + 454 * u) >> 8, 255);
+      rgb[index * 3 + 3] = minmax(0, (y1 + 359 * v) >> 8, 255);
+      rgb[index * 3 + 4] = minmax(0, (y1 + 88 * v - 183 * u) >> 8, 255);
+      rgb[index * 3 + 5] = minmax(0, (y1 + 454 * u) >> 8, 255);
+    }
+  }
+  return rgb;
+}
