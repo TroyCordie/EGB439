6996ec91f9586dc6c50851998293b95fe3902e07
Peter Corke <peter.i.corke@gmail.com>
2017-04-09 16:57:33 +1000

    Add support for move with profile command Add command line option for debug Minor network code tweaks

--- python/server-motors.py
+++ python/server-motors.py
@@ -6,6 +6,7 @@ import socket
 import sys
 import threading
 import os
+import argparse
 
 import penguinPi as ppi
 
@@ -17,45 +18,96 @@ PORT = 43900
 CHUNK_SIZE = 128
 
 FN_MOTOR_SPEEDS = 'setMotorSpeeds'
+FN_MOTOR_SPEEDS_PROFILE = 'setMotorSpeedsProfile'
 FN_MOTOR_TICKS = 'getMotorTicks'
 FN_MOTOR_ENCODERS = 'getMotorEncoders'
 FN_DISPLAY_VALUE = 'setDisplayValue'
 FN_DISPLAY_MODE = 'setDisplayMode'
 FN_ALL_STOP = 'stopAll'
 
-debug = False;
 
 """
 " Helper functions
 """
+def setspeed(speed, fraction):
+	global mA, mB
+
+	mA.set_power(int(speed[0]*fraction))
+	mB.set_power(int(speed[1]*fraction))
+
+def xfrange(start, stop, step):
+    i = 0
+    while start + i * step < stop:
+        yield start + i * step
+        i += 1
+
 def executeRequestedFunction(requestData, connection):
-	if debug:
+	if args.debug:
 		print('Function request: "{0}"' .format(requestData), file=sys.stderr)
 
 	# Splice and dice
 	data = requestData.decode("utf-8").split(',')
 	fn = data[0]
 
-	if debug:
+	if args.debug:
 		print('data contains ')
 		print(data)
 
 	# Decide what function should be run, and attempt to run it with the arguments
 	if fn == FN_MOTOR_SPEEDS:
+		# set motor speed
+		if args.debug:
+			print("set motor speed")
 		motorA = int(data[1])
 		motorB = int(data[2])
 
 		mA.set_power(motorA)
 		mB.set_power(motorB)
 
+	elif fn == FN_MOTOR_SPEEDS_PROFILE:
+		# move motors by specified amount
+		if args.debug:
+			print("set motor speed with profile:", data)
+
+		# there are four arguments: speedA, speedB, duration, acceltime
+		speed = [ int(x) for x in data[1:3] ]
+		Ttotal = float(data[3])
+		Taccel = float(data[4])
+
+		dt = 0.05;
+		if Taccel > 0:
+			for t in xfrange(0, Taccel, dt):
+				setspeed(speed, t/Taccel);
+				time.sleep(dt)
+
+		for t in xfrange(0, Ttotal-Taccel, dt):
+			setspeed(speed, 1.0)
+			time.sleep(dt)
+
+		if Taccel > 0:
+			for t in xfrange(0, Taccel, dt):
+				setspeed(speed, (Taccel-t)/Taccel);
+				time.sleep(dt)
+
+		# all stop
+		mA.set_power(0)
+		mB.set_power(0)
+
+		# signal that we're done here
+		s = 'done\n'
+		b = s.encode('utf-8')
+
+		# send completion back over 'connection
+		connection.sendall(b)
+
 	elif fn == FN_MOTOR_TICKS:
-		if debug:
+		if args.debug:
 			print("read ticks")
 		mAticks = mA.get_ticks()
 		mBticks = mB.get_ticks()
 		s = str(mAticks) + ' ' + str(mBticks) + ' ' + '\n'
 
-		if debug:
+		if args.debug:
 			print(s)
 
 		b = s.encode('utf-8')
@@ -64,13 +116,13 @@ def executeRequestedFunction(requestData, connection):
 		connection.sendall(b)
 
 	elif fn == FN_MOTOR_ENCODERS:
-		if debug:
+		if args.debug:
 			print("read encoders")
 		mAenc = mA.get_encoder()
 		mBenc = mB.get_encoder()
 		s = str(mAenc) + ' ' + str(mBenc) + ' ' + '\n'
 
-		if debug:
+		if args.debug:
 			print(s)
 
 		b = s.encode('utf-8')
@@ -95,18 +147,25 @@ def executeRequestedFunction(requestData, connection):
 """
 " Heartbeat thread, pulse the green LED periodically
 """
-def HeartBeat:
+def HeartBeat():
 
-    led = LED(AD_LED_G)
+    led = ppi.LED(ppi.AD_LED_G)
 
     while True:
         led.set_state(1);
         led.set_count(1000);
-        time.sleep(5);
+        time.sleep(2);
 
 """
-" Main exectution block
+" Main execution block
 """
+import argparse
+# handle command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--debug", help="show debug information", 
+	action="store_true")
+args = parser.parse_args()
+
 # Create a TCP/IP socket and bind the socket to the port
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = (IP_ADDRESS, PORT)
@@ -131,10 +190,7 @@ client_address = None
 heartbeat_thread = threading.Thread(target=HeartBeat, daemon=True)
 heartbeat_thread.start()
 
-# Start listening for incoming commands
-sock.setblocking(0)
-sock.listen(1)
-
+# attempt to get the low byte of the WLAN IP address
 try:
     f = os.popen('sudo ifconfig wlan0 | grep "inet\ addr" | cut -d: -f2 | cut -d" " -f1')
     ip = f.read()
@@ -145,29 +201,19 @@ except ValueError:
     display.set_mode('d')
     display.set_value(-1);
 
+## handle incoming commands
 
-
+#sock.setblocking(0)
+sock.listen(1)
 
 while True:
-	# Check if any new commands have been received
-	try: # try catch used because error thrown when there's connection - i.e. no signal transmitted
-		connection, client_address = sock.accept()
+	connection, client_address = sock.accept()
+	recv_str = connection.recv(CHUNK_SIZE)
+	data = recv_str
+	while not recv_str:
 		recv_str = connection.recv(CHUNK_SIZE)
-		data = recv_str
-		while not recv_str:
-			recv_str = connection.recv(CHUNK_SIZE)
-			data += recv_str
-		# TODO should check if all data was received.... but too lazy
+		data += recv_str
+	# TODO should check if all data was received.... but too lazy
 
-		# Send the data to the function processor
-		executeRequestedFunction(data.rstrip(), connection)
-	except socket.error as msg:
-        print('socket error: ', msg.value);
-		pass
-    except:
-        print('command parser failed: ', data);
-        pass
-	finally:
-		# Clean up the connection if it exists
-		if connection is not None:
-			connection.close()
+	# Send the data to the function processor
+	executeRequestedFunction(data.rstrip(), connection)
