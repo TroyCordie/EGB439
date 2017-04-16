5b83f69764dd54ddf4b60a9f9d8b95812d0436aa
Peter Corke <peter.i.corke@gmail.com>
2017-04-16 12:49:46 +1000

    Add options to change color balance mode or manually set color channel gains

--- python/server-camera.py
+++ python/server-camera.py
@@ -4,6 +4,7 @@ import time
 import socket
 import sys
 import io
+import argparse
 
 import picamera
 #from BrickPi import *
@@ -53,6 +54,25 @@ def executeRequestedFunction(requestData, connection):
 """
 " Main execution block
 """
+
+# parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-a", "--auto", dest="awb", help="auto white balance", 
+	action="store_const", const="auto")
+parser.add_argument("-o", "--off", dest="awb", help="disable white balance", 
+	action="store_const", const="off")
+parser.add_argument("-t", "--tungsten", dest="awb", help="tungsten white balance", 
+	action="store_const", const="tungsten")
+parser.add_argument("-s", "--sun", dest="awb", help="sun white balance", 
+	const="sunlight", action="store_const")
+parser.add_argument("-g", "--gain", dest="gain", action="store", help="set white balance gain manually: rbgain OR rgain,bgain")
+args = parser.parse_args()
+if args.gain:
+	args.awb = 'off'
+
+print(args)
+print(args.awb)
+
 # Create a TCP/IP socket and bind the socket to the port
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_address = (IP_ADDRESS, PORT)
@@ -65,6 +85,11 @@ stream = io.BytesIO()
 camera = picamera.PiCamera()
 camera.resolution = (IM_WIDTH, IM_HEIGHT)
 #camera.start_preview()
+if args.awb:
+	camera.awb_mode = args.awb
+if args.gain:
+	camera.awb_gains = tuple(float(x) for x in args.gain.split(','))
+print('white balance mode is ', camera.awb_mode)
 time.sleep(2)
 
 # Initialise loop variables
